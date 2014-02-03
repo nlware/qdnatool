9adda9b4a99ce02abcaf39173b2b553338b7ef29
Mark van Driel <mvdriel@mvdriel-EP35-DS3R>
2014-02-03 14:23:48 +0100

    Added initial empty Test for Model Exam

---
+++ app/Test/Case/Model/ExamTest.php
@@ -0,0 +1,203 @@
+<?php
+App::uses('Exam', 'Model');
+
+/**
+ * Exam Test Case
+ *
+ */
+class ExamTest extends CakeTestCase {
+
+/**
+ * Fixtures
+ *
+ * @var array
+ */
+	public $fixtures = array();
+
+/**
+ * setUp method
+ *
+ * @return void
+ */
+	public function setUp() {
+		parent::setUp();
+		$this->Exam = ClassRegistry::init('Exam');
+	}
+
+/**
+ * tearDown method
+ *
+ * @return void
+ */
+	public function tearDown() {
+		unset($this->Exam);
+
+		parent::tearDown();
+	}
+
+/**
+ * testAdd method
+ *
+ * @return void
+ */
+	public function testAdd() {
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+/**
+ * testRemove method
+ *
+ * @return void
+ */
+	public function testRemove() {
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+/**
+ * testScheduleAnalyse method
+ *
+ * @return void
+ */
+	public function testScheduleAnalyse() {
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+/**
+ * testAnalyse method
+ *
+ * @return void
+ */
+	public function testAnalyse() {
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+/**
+ * testScheduleReport method
+ *
+ * @return void
+ */
+	public function testScheduleReport() {
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+/**
+ * testReport method
+ *
+ * @return void
+ */
+	public function testReport() {
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+/**
+ * testImport method
+ *
+ * @return void
+ */
+	public function testImport() {
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+/**
+ * testImportBlackboard method
+ *
+ * @return void
+ */
+	public function testImportBlackboard() {
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+/**
+ * testImportQMP method
+ *
+ * @return void
+ */
+	public function testImportQMP() {
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+/**
+ * testImportTeleform method
+ *
+ * @return void
+ */
+	public function testImportTeleform() {
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+/**
+ * testStevie method
+ *
+ * @return void
+ */
+	public function testStevie() {
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+/**
+ * testScheduleReanalyse method
+ *
+ * @return void
+ */
+	public function testScheduleReanalyse() {
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+/**
+ * testReanalyse method
+ *
+ * @return void
+ */
+	public function testReanalyse() {
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+/**
+ * testScores method
+ *
+ * @return void
+ */
+	public function testScores() {
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+/**
+ * testMissings method
+ *
+ * @return void
+ */
+	public function testMissings() {
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+}
