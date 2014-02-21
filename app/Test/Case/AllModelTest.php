e50efe09496a62f3c8fe78c0ab6d7fce34814983
Mark van Driel <mvdriel@nlware.com>
2014-02-21 16:35:16 +0100

    Added initial Test suites

---
+++ app/Test/Case/AllModelTest.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * AllModelTest class
+ *
+ * This test group will run Model related tests.
+ */
+class AllModelTest extends CakeTestSuite {
+
+/**
+ * suite method, defines tests for this suite.
+ *
+ * @return void
+ */
+	public static function suite() {
+		$suite = new CakeTestSuite('All Model tests');
+		$suite->addTestDirectory(TESTS . 'Case/Model');
+		return $suite;
+	}
+}
