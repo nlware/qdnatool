ae763e9c8e353a685f5deba5cab86b21d3d27bfc
Mark van Driel <mvdriel@nlware.com>
2014-03-11 11:01:46 +0100

    Refactoring: use constant DS as directory separator

--- app/Test/Case/AllModelTest.php
+++ app/Test/Case/AllModelTest.php
@@ -13,7 +13,7 @@ class AllModelTest extends CakeTestSuite {
  */
 	public static function suite() {
 		$suite = new CakeTestSuite('All Model tests');
-		$suite->addTestDirectory(TESTS . 'Case/Model');
+		$suite->addTestDirectory(TESTS . 'Case' . DS . 'Model');
 		return $suite;
 	}
 }
