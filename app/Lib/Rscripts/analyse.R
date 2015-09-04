035ded30b3f2e0f5972ba9489497a97cfc6c13a5
Sjoerd Glaser <s.s.f.glaser@uva.nl>
2015-09-04 16:05:27 +0200

    Analyse + markdown update
    Analyse update + Markdown HTML script (HTML needs to be changed to
    „normal” script)

--- app/Lib/Rscripts/analyse.R
+++ app/Lib/Rscripts/analyse.R
@@ -1,3 +1,13 @@
+setwd("//Volumes//fmg-public//Psychologie//Onderwijsinstituut//Psychologie tweedejaarspracticumgroepen//Persoonlijke Folders//Sjoerd//qDNA Tool")
+input.answers = read.table("Test.txt",sep = ",")
+input.answers = input.answers[,3:22]
+input.answers = input.answers[1,1:3]
+key = key[,1:3]
+input.answers[input.answers == 9] <- 0
+Nquest = ncol(input.answers)
+n.answer.op <- rep(4,Nquest)
+category = sample(1:3,Nquest,replace = TRUE)
+# Test toevoeging door Sjoerd
 # Get necessary packages
 library(psy)
 
@@ -9,30 +19,34 @@ Analyse <- function(key, input.answers, n.answer.op) {
   input.correct <- matrix(0, n.stud, n.item) 
   
   # Fill in Correct/Incorrect Matrix
-  for (j in 1: ncol(input.answers)) {
-    if (any(key[, j] != 0)) {  # If no key is supplied for a question, item is seen as manually graded and input.answers is used directly
-      input.correct[, j] <- as.numeric(input.answers[, j] %in% which(key[, j] == 1))
+  for (j in 1: n.item) {
+    for (i in 1: n.stud) {
+      if (!is.null(input.answers[i, j]) & all(key[, j] == 0)) {
+        input.correct[i, j] <- input.answers[i, j]
+      } else if(any(input.answers[i, j] == which(key[, j] == 1))) {
+        input.correct[i, j] <- 1
+      }
     }
   }
   
   # Creating Frequency Matrix and Item rest Cor for total scores
-  item.sum <- colSums(input.correct)
+  item.sum <- apply(input.correct, 2, sum)
   item.perc <- round(item.sum / n.stud * 100, digits = 1)
   
   if (n.item > 2 & n.stud > 1) {
-    item.tot.cor <- numeric()
-    suppressWarnings(  # If no one or everyone answered an item correctly, R returns NA and a warning
+    corrected.item.tot.cor <- numeric()
+    suppressWarnings(
       for (j in 1 : n.item) {
-        item.tot.cor <- c(item.tot.cor,
+        corrected.item.tot.cor <- c(corrected.item.tot.cor,
                                     cor(input.correct[, j],
                                         apply(input.correct[, -j], 1, sum)))
       }
     )
     
-    item.tot.cor[is.na(item.tot.cor)] <- 0 # Correct for when all students answered correctly or incorrectly
-    item.tot.cor <- round(item.tot.cor, digits = 3)
+    corrected.item.tot.cor[is.na(corrected.item.tot.cor)] <- 0
+    corrected.item.tot.cor <- round(corrected.item.tot.cor, digits = 3)
   } else {
-    item.tot.cor <- 0
+    corrected.item.tot.cor <- 0
   }
   
   # Creating Frequency Matrix and Item rest Cor for each answer options
@@ -41,17 +55,24 @@ Analyse <- function(key, input.answers, n.answer.op) {
   if (any(key != 0)) {
     freq.answer.op <- matrix(0, max(n.answer.op) + 1,
                              n.item)
-    for (j in 1 : n.item){
-      if (any(key[, j] != 0)){
-        freq.answer.op[,j] <- table(factor(input.answers[, j], levels = 0:max(n.answer.op)))
-      }
+    for (i in 0 : max(n.answer.op)) {
+      for (j in 1 : n.item) {
+        if (any(key[, j] != 0)) 
+          freq.answer.op[i + 1, j] <- sum(input.answers[, j] == i)
+      } 
     }
     
-    rownames <- c("Times_Answer_Missing", paste("Times", LETTERS[1:max(n.answer.op)], "answered", sep = "_"))
+    rownames <- "Times_Answer_Missing"
+    for (i in 1: max(n.answer.op)) {
+      rownames <- c(rownames, paste(c("Times_", LETTERS[i], "_answered"),
+                                    collapse = ""))
+    } 
+    
     rownames(freq.answer.op) <- rownames
     
     # Percentage answered per answer option per questions
-    perc.answer.op <- round(freq.answer.op / n.stud * 100, digits = 1)
+    perc.answer.op <- round(freq.answer.op / n.stud * 100,
+                            digits = 1)
     
     # Calculating corrected item total correlation per Answeroptions
     if(n.item > 2 & n.stud > 1){
@@ -62,8 +83,9 @@ Analyse <- function(key, input.answers, n.answer.op) {
         for (i in 0:max(n.answer.op)) {
           for (j in 1:n.item) {
             if (any(key[, j] != 0)) {
-              answer.op.tot.cor[i + 1, j] =
-                round(cor(as.numeric(input.answers[, j] == i), rowSums(input.correct[, -j])), digits = 3)
+              answer.op.tot.cor[i + 1, j]=
+                round(cor(as.numeric(input.answers[, j] == i),
+                          apply(input.correct[, -j], 1, sum)), digits = 3)
               if (is.na(answer.op.tot.cor[i + 1, j])) {
                 answer.op.tot.cor[i + 1, j] <- 0
               }
@@ -73,7 +95,7 @@ Analyse <- function(key, input.answers, n.answer.op) {
           }
         }
       )
-      rownames(answer.op.tot.cor) <- rownames
+    rownames(answer.op.tot.cor) <- rownames
     } else {
       answer.op.tot.cor <- 0
     }
