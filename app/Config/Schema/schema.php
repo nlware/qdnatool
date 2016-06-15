4d446f231d700b7410c64730c5fd5b66765a575b
Mark van Driel <mvdriel@nlware.com>
2016-06-15 14:26:35 +0200

    Added field max_answer_option_count to table domains

--- app/Config/Schema/schema.php
+++ app/Config/Schema/schema.php
@@ -307,6 +307,7 @@ class AppSchema extends CakeSchema {
 		'exam_id' => array('type' => 'integer', 'null' => false, 'default' => null, 'length' => 10, 'unsigned' => true, 'key' => 'index'),
 		'name' => array('type' => 'string', 'null' => false, 'default' => null, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
 		'cronbachs_alpha' => array('type' => 'decimal', 'null' => true, 'default' => null, 'length' => '4,3', 'unsigned' => false),
+		'max_answer_option_count' => array('type' => 'integer', 'null' => true, 'default' => null, 'length' => 10, 'unsigned' => true),
 		'indexes' => array(
 			'PRIMARY' => array('column' => 'id', 'unique' => 1),
 			'exam_id' => array('column' => 'exam_id', 'unique' => 0)
--- app/Test/Fixture/DomainFixture.php
+++ app/Test/Fixture/DomainFixture.php
@@ -14,6 +14,7 @@ class DomainFixture extends CakeTestFixture {
 		'exam_id' => array('type' => 'integer', 'null' => false, 'default' => null, 'length' => 10, 'unsigned' => true, 'key' => 'index'),
 		'name' => array('type' => 'string', 'null' => false, 'default' => null, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
 		'cronbachs_alpha' => array('type' => 'decimal', 'null' => true, 'default' => null, 'length' => '4,3', 'unsigned' => false),
+		'max_answer_option_count' => array('type' => 'integer', 'null' => true, 'default' => null, 'length' => 10, 'unsigned' => true),
 		'indexes' => array(
 			'PRIMARY' => array('column' => 'id', 'unique' => 1),
 			'exam_id' => array('column' => 'exam_id', 'unique' => 0)
@@ -31,13 +32,15 @@ class DomainFixture extends CakeTestFixture {
 			'id' => 1,
 			'exam_id' => 1,
 			'name' => 'A',
-			'cronbachs_alpha' => null
+			'cronbachs_alpha' => null,
+			'max_answer_option_count' =>null
 		),
 		array(
 			'id' => 2,
 			'exam_id' => 1,
 			'name' => 'B',
-			'cronbachs_alpha' => null
+			'cronbachs_alpha' => null,
+			'max_answer_option_count' =>null
 		),
 	);
 
