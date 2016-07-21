ee19dda0a713a4f3400d796270b24e8f5fef0a3b
Mark van Driel <mvdriel@nlware.com>
2016-07-21 15:24:14 +0200

    Add tests (#50)

--- app/Test/Case/Lib/RserveTest.php
+++ app/Test/Case/Lib/RserveTest.php
@@ -1,11 +1,20 @@
 <?php
 App::uses('Rserve', 'Lib');
+App::uses('Folder', 'Utility');
 
 /**
  * Rserve Test Case
  *
  */
 class RserveTest extends CakeTestCase {
+
+	private $__tmpPath = null;
+
+	public function __construct() {
+		parent::__construct();
+
+		$this->__tmpPath = TMP . 'tests' . DS . 'Lib' . DS . 'Rserve';
+	}
 
 /**
  * setUp method
@@ -16,7 +25,11 @@ class RserveTest extends CakeTestCase {
 		$this->skipIf(!file_exists(APP . 'Config' . DS . 'rserve.php'), 'Rserve configuration file is NOT present.');
 
 		parent::setUp();
+
 		$this->Rserve = new Rserve();
+
+		$folder = new Folder();
+		$folder->create($this->__tmpPath);
 	}
 
 /**
@@ -26,7 +39,11 @@ class RserveTest extends CakeTestCase {
  */
 	public function tearDown() {
 		parent::tearDown();
+
 		unset($this->Rserve);
+
+		$folder = new Folder();
+		$folder->delete($this->__tmpPath);
 	}
 
 /**
@@ -139,11 +156,11 @@ class RserveTest extends CakeTestCase {
 	}
 
 /**
- * testExecuteRscriptsAnalyseToLittleItems method
+ * testExecuteRscriptsAnalyseTooLittleItems method
  *
  * @return void
  */
-	public function testExecuteRscriptsAnalyseToLittleItems() {
+	public function testExecuteRscriptsAnalyseTooLittleItems() {
 		$script = array();
 		$script[] = file_get_contents(APP . 'Lib' . DS . 'Rscripts' . DS . 'analyse.R');
 		$script[] = 'nvragen = 2;';
@@ -158,11 +175,11 @@ class RserveTest extends CakeTestCase {
 	}
 
 /**
- * testExecuteRscriptsAnalyseToLittleItems method
+ * testExecuteRscriptsAnalyseTooLittleItems method
  *
  * @return void
  */
-	public function testExecuteRscriptsAnalyseToLittleStudents() {
+	public function testExecuteRscriptsAnalyseTooLittleStudents() {
 		$script = array();
 		$script[] = file_get_contents(APP . 'Lib' . DS . 'Rscripts' . DS . 'analyse.R');
 		$script[] = 'nvragen = 3;';
@@ -174,6 +191,47 @@ class RserveTest extends CakeTestCase {
 		$script = implode("\n", $script);
 		$result = $this->Rserve->execute($script);
 		$this->assertFalse($result);
+	}
+
+/**
+ * testExecuteRscriptsReport method
+ *
+ * @return void
+ */
+	public function testExecuteRscriptsReport() {
+		$filename = $this->__tmpPath . DS . 'testExecuteRscriptsReport.pdf';
+
+		$this->assertFileNotExists($filename);
+
+		$script = array();
+		$script[] = file_get_contents(APP . 'Lib' . DS . 'Rscripts' . DS . 'report.R');
+		$script[] = sprintf('file.name = "%s";', $filename);
+		$script[] = 'number.students = 2;';
+		$script[] = 'number.answeroptions = c(3,3,3);';
+		$script[] = 'number.questions = 3;';
+		$script[] = 'cronbach = 0.5;';
+		$script[] = 'frequency.answer.options = matrix(c(1,0,0,1,0,0,1,0,0,1,0,0), nrow = 4, ncol = number.questions, byrow = FALSE);';
+		$script[] = 'percentage.answer.options = matrix(c(1,0,0,1,0,0,1,0,0,1,0,0), nrow = 4, ncol = number.questions, byrow = FALSE);';
+		$script[] = 'input.correct = matrix(c(0,0,0,0,0,0), nrow = number.students, ncol = number.questions, byrow = TRUE);';
+		$script[] = 'key = matrix(c(1,0,0,1,0,0,1,0,0), nrow = 3, ncol = number.questions, byrow = FALSE);';
+		$script[] = 'correct.frequency = c(0,0,0);';
+		$script[] = 'correct.percentage = c(0,0,0);';
+		$script[] = 'corrected.item.tot.cor = 3;';
+		$script[] = 'corrected.item.tot.cor.answ.option = matrix(c(1,0,0,1,0,0,1,0,0,1,0,0), nrow = 4, ncol = number.questions, byrow = FALSE);';
+		$script[] = 'title = "Test Title";';
+		$script[] = 'item.names = c("Item 1","Item 2","Item 3");';
+		$script[] = 'GenerateReport(file.name, number.students, number.answeroptions, number.questions, cronbach,
+			frequency.answer.options, percentage.answer.options,
+			input.correct, key, correct.frequency,
+			correct.percentage, corrected.item.tot.cor,
+			corrected.item.tot.cor.answ.option, title,
+			item.names);';
+
+		$script = implode("\n", $script);
+		$result = $this->Rserve->execute($script);
+		$this->assertTrue((bool)$result);
+
+		$this->assertFileExists($filename);
 	}
 
 }
