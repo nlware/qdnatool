b7b6fa13a1242264302aa29a4fbf3d79d16d7221
Mark van Driel <mvdriel@nlware.com>
2016-08-10 10:30:03 +0200

    Updated R scripts

--- app/Lib/Rscripts/report.R
+++ app/Lib/Rscripts/report.R
@@ -1,17 +1,10 @@
-report <- function(file.name,
-                   title,
-                   number.answeroptions,
-                   key,
-                   item.names,
-                   correct.frequency,
-                   correct.percentage,
-                   frequency.answer.options,
-                   percentage.answer.options,
-                   corrected.item.tot.cor,
-                   corrected.item.tot.cor.answ.option,
-                   cronbach,
-                   student.scores,
-                   categories) {
-  rmarkdown::render("report.Rmd", output_format = "html_document", output_file = paste0(file.name,".html"))
-  rmarkdown::render("report.Rmd", output_format = "pdf_document", output_file = paste0(file.name,".pdf"))
+report <- function(filename, title, number_answeroptions, key, item_names,
+                   correct_frequency, correct_percentage,
+                   frequency_answer_options, percentage_answer_options,
+                   corrected_item_tot_cor, corrected_item_tot_cor_answ_option,
+                   cronbach, student_scores, categories) {
+  rmarkdown::render("report_subcategories.Rmd", output_format = "html_document",
+                    output_file = paste0(filename, ".html"))
+  rmarkdown::render("report_subcategories.Rmd", output_format = "pdf_document",
+                    output_file = paste0(filename, ".pdf"))
 }
