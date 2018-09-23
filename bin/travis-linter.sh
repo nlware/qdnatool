d01241d1d2aefd2b877a39322021589b284d46c9
Mark van Driel <mvdriel@nlware.com>
2018-09-23 11:55:28 +0200

    Fix Travis build

--- bin/travis-linter.sh
+++ bin/travis-linter.sh
@@ -3,6 +3,8 @@ set -e
 
 exitstatus=0
 
+Rscript -e 'if(!"lintr" %in% rownames(installed.packages())) { install.packages("lintr", dependencies = TRUE) }'
+
 for file in $(pwd)/app/Lib/Rscripts/*.R
 do
     Rscript -e "lintr::lint(\"$file\")"
